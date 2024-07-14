dbaeca9300e671a5570affb5284b796219fe965d
DeveloperJoshi <developerjoshifunctionae@gmail.com>
2024-07-14 10:24:53 +0000

    Pointers program on how they work.

---
+++ pointersreferebce.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int x = 10;
+  int *ptr = &x;
+  cout << "Value of x is " << x << endl;
+  cout << "Value of ptr is " << ptr << endl;
+}
